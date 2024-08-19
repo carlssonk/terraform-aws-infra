47e6bfcd8903b57757bb9f1a2306536a9fc9126c
carlssonk <o.carlssonk@gmail.com>
2024-08-19 21:18:18 +0200

    Added more required permissions to base policy

--- bootstrap/setup-oidc/main.tf
+++ bootstrap/setup-oidc/main.tf
@@ -8,8 +8,12 @@ terraform {
   backend "s3" {}
 }
 
+locals {
+  domain = "token.actions.githubusercontent.com"
+}
+
 resource "aws_iam_openid_connect_provider" "github_actions" {
-  url             = "https://token.actions.githubusercontent.com"
+  url             = "https://${local.domain}"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
@@ -27,10 +31,10 @@ resource "aws_iam_role" "terraform_execution_role" {
       }
       Condition = {
         StringEquals = {
-          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
+          "${local.domain}:aud": "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub": "repo:${var.organization}/${var.repository}:*"
+          "${local.domain}.com:sub": "repo:${var.organization}/${var.repository}:*"
         }
       }
     }]
@@ -68,14 +72,32 @@ resource "aws_iam_policy" "terraform_base_policy" {
         Resource: "arn:aws:dynamodb:*:*:table/${var.organization}-${var.backend_table_name}-${var.environment}"
       },
       {
-        // Enables user to update itself with new policies
+        // Required for communicating with OpenID Connect Provider
+        Effect: "Allow",
+        Action: [
+          "iam:GetOpenIDConnectProvider",
+        ],
+        Resource: "arn:aws:iam::*:oidc-provider/${local.domain}"
+      },
+      {
+        // Enables policy to get itself
+        Effect: "Allow",
+        Action: [
+          "iam:GetPolicy",
+        ],
+        Resource: self.arn
+      },
+      {
+        // Enables role to add policies to itself
         Effect = "Allow"
         Action = [
           "iam:GetRole",
           "iam:UpdateAssumeRolePolicy",
           "iam:UpdateRole",
           "iam:AttachRolePolicy",
-          "iam:DetachRolePolicy"
+          "iam:DetachRolePolicy",
+          "iam:ListRolePolicies",
+          "iam:ListAttachedRolePolicies"
         ]
         Resource = aws_iam_role.terraform_execution_role.arn
       }
