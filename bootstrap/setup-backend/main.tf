70e0de51d019fcaa672112482bc9e4f44aa22ede
carlssonk <o.carlssonk@gmail.com>
2024-08-19 15:57:06 +0200

    Fixed resource path + added bootstrap user policy

--- bootstrap/setup-backend/main.tf
+++ bootstrap/setup-backend/main.tf
@@ -9,11 +9,11 @@ provider "aws" {
 }
 
 module "s3" {
-  source = "../modules/s3"
+  source = "../../modules/s3"
   bucket_name = "${var.organization}-terraform-state-bucket-${var.environment}"
 }
 
 module "dynamodb" {
-  source = "../modules/dynamodb"
+  source = "../../modules/dynamodb"
   table_name = "${var.organization}-terraform-lock-table-${var.environment}"
 }
