f098f16efaf2f1975edbcda0c66771c681c64ca2
carlssonk <o.carlssonk@gmail.com>
2024-08-22 01:30:12 +0200

    Added required_providers

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.7"
+    }
+  }
+}
+terraform {
   backend "s3" {}
 }
 
