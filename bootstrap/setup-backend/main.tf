a8f807c70982d39d3557653dba78a8e26ab3f282
carlssonk <o.carlssonk@gmail.com>
2024-08-21 23:09:26 +0200

    Fixed circular dependency

--- bootstrap/setup-backend/main.tf
+++ bootstrap/setup-backend/main.tf
@@ -7,13 +7,17 @@ provider "aws" {
   region = var.region
 }
 
-module "s3" {
-  source = "../../modules/s3"
-  bucket_name = "terraform-state-bucket"
-  is_bootstrap_user = true
+resource "aws_s3_bucket" "this" {
+  bucket = "${var.organization}-terraform-state-bucket-${terraform.workspace}"
 }
 
-module "dynamodb" {
-  source = "../../modules/dynamodb"
-  table_name = "terraform-lock-table"
+resource "aws_dynamodb_table" "this" {
+  name = "${var.organization}-terraform-lock-table-${terraform.workspace}"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
