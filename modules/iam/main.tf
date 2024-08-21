4499281a9bfeefd4da8aab791c4fc095b6bd2cea
carlssonk <o.carlssonk@gmail.com>
2024-08-22 00:04:47 +0200

    merge fix

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,32 +1,48 @@
 variable "name" {}
 variable "policy_document" {}
 
-data "aws_iam_policy" "existing" {
-  name = "terraform-${var.name}-policy"
+data "aws_iam_policy" "existing_policy" {
+  count = can(data.aws_iam_policy_document.policy_exists.json) ? 1 : 0
+  name  = var.name
+}
+
+data "aws_iam_policy_document" "policy_exists" {
+  statement {
+    sid       = "CheckIfPolicyExists"
+    effect    = "Allow"
+    actions   = ["iam:GetPolicy"]
+    resources = ["arn:aws:iam::*:policy/terraform-${var.name}-policy"]
+  }
 }
 
 locals {
-  policy_exists = data.aws_iam_policy.existing.arn != ""
+  existing_policy = try(data.aws_iam_policy.existing_policy[0].policy, "{}")
+  new_policy      = var.policy_document
+  
+  existing_policy_doc = jsondecode(local.existing_policy)
+  new_policy_doc      = jsondecode(local.new_policy)
+  
+  merged_statements = distinct(concat(
+    try(local.existing_policy_doc.Statement, []),
+    try(local.new_policy_doc.Statement, [])
+  ))
+  
+  merged_policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = local.merged_statements
+  })
 }
 
-data "aws_iam_policy_document" "merged" {
-  source_policy_documents = [
-    local.policy_exists ? data.aws_iam_policy.existing.policy : null,
-    var.policy_document
-  ]
-}
+resource "aws_iam_policy" "policy" {
+  name   = var.name
+  policy = local.merged_policy
 
-resource "aws_iam_policy" "this" {
-  name        = "terraform-${var.name}-policy"
-  description = "Composite policy for Terraform execution role"
-  policy      = data.aws_iam_policy_document.merged.json
-
-  lifecycle {
+   lifecycle {
     create_before_destroy = true
   }
 }
 
-resource "aws_iam_role_policy_attachment" "this" {
+resource "aws_iam_role_policy_attachment" "attachment" {
   role       = "terraform-execution-role"
-  policy_arn = aws_iam_policy.this.arn
+  policy_arn = aws_iam_policy.policy.arn
 }
