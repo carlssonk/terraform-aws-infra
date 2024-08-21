08a26041eeef1fe1348e3f8eb4e41240ce422d7e
carlssonk <o.carlssonk@gmail.com>
2024-08-22 01:19:35 +0200

    Only try to create the policy

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -13,17 +13,17 @@ module "iam" {
   is_public_website = var.is_public_website
 }
 
-resource "time_sleep" "wait_for_iam" {
-  create_duration = "15s"
-}
+# resource "time_sleep" "wait_for_iam" {
+#   create_duration = "15s"
+# }
 
-module "resources" {
-  source = "./resources"
-  depends_on = [time_sleep.wait_for_iam]
+# module "resources" {
+#   source = "./resources"
+#   depends_on = [time_sleep.wait_for_iam]
 
-  bucket_name_full = local.bucket_name_full
-  is_public_website = var.is_public_website
-}
+#   bucket_name_full = local.bucket_name_full
+#   is_public_website = var.is_public_website
+# }
 
 output "policy_document" {
   value = module.iam.policy_document
