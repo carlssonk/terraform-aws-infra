2d4168526418cc1e941d4b5592e2b1fdd2b8ec89
carlssonk <o.carlssonk@gmail.com>
2024-08-21 23:17:37 +0200

    Test multiple resources

--- apps/portfolio/main.tf
+++ apps/portfolio/main.tf
@@ -3,3 +3,9 @@ module "s3" {
   bucket_name = "portfolio"
   is_public_website = true
 }
+
+module "s3" {
+  source = "../../modules/s3"
+  bucket_name = "portfolio-2"
+  is_public_website = true
+}
