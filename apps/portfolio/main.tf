99e0209d4f1e50c8bb62921fd4d386d1499e703d
carlssonk <o.carlssonk@gmail.com>
2024-08-22 01:48:01 +0200

    Remove second portfolio bucket

--- apps/portfolio/main.tf
+++ apps/portfolio/main.tf
@@ -4,14 +4,8 @@ module "s3" {
   is_public_website = true
 }
 
-module "s3_2" {
-  source = "../../modules/s3"
-  bucket_name = "portfolio-2"
-  is_public_website = true
-}
-
 module "iam" {
   source = "../../modules/iam"
   name = "portfolio"
-  policy_documents = [module.s3.policy_document, module.s3_2.policy_document]
+  policy_documents = [module.s3.policy_document]
 }
