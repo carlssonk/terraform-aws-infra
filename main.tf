6f41abd9295586b432b777c13cfeff09c222428a
carlssonk <o.carlssonk@gmail.com>
2024-08-22 21:17:16 +0200

    Removed unecessary providers

--- main.tf
+++ main.tf
@@ -1,16 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "~> 0.7"
-    }
-  }
-}
-terraform {
   backend "s3" {}
 }
 
