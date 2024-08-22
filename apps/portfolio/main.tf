ba01226e879aa93b135a1fb6e99d0020fb2e26ac
carlssonk <o.carlssonk@gmail.com>
2024-08-23 00:20:03 +0200

    Rename local modules

--- apps/portfolio/main.tf
+++ apps/portfolio/main.tf
@@ -2,15 +2,15 @@ module "globals" {
   source = "../../globals"
 }
 
-module "asd" {
+module "s3_bucket" {
   source = "../../modules/s3"
   bucket_name = "portfolio"
   is_public_website = true
 }
 
-module "create_policy" {
+module "policy" {
   count = module.globals.var.workflow_step == "iam" ? 1 : 0
   source = "../../modules/iam"
   name = "portfolio"
-  policy_documents = [module.asd.policy_document]
+  policy_documents = [module.s3_bucket.policy_document]
 }
