caf718c7df261a70cc12949af108e1bd8e06fd3d
carlssonk <o.carlssonk@gmail.com>
2024-08-22 20:15:52 +0200

    changes

--- modules/dynamodb/main.tf
+++ modules/dynamodb/main.tf
@@ -1,28 +1,25 @@
-module "globals" {
-  source = "../../global_constants"
+data "local_file" "globals" {
+  filename = "${path.root}/globals.json"
 }
 
 locals {
+  globals = jsondecode(data.local_file.globals.content)
+  workflow_step = local.globals.workflow_step
   table_name_full = "${module.globals.organization}-${var.table_name}-${terraform.workspace}"
 }
 
-module "iam" {
+module "generate_policy_document" {
+  count = workflow_step == "iam" ? 1 : 0
   source = "./iam"
+  table_name_full = local.table_name_full
+}
 
+module "resources" {
+  count = workflow_step == "resources" ? 1 : 0
+  source = "./resources"
   table_name_full = local.table_name_full
 }
 
 output "policy_document" {
-  value = module.iam.policy_document
-}
-
-resource "time_sleep" "wait_for_iam" {
-  create_duration = "15s"
-}
-
-module "resources" {
-  source = "./resources"
-  depends_on = [time_sleep.wait_for_iam]
-
-  table_name_full = local.table_name_full
+  value = module.generate_policy_document[0].policy_document
 }
--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -11,20 +11,17 @@ locals {
 module "generate_policy_document" {
   count = local.workflow_step == "iam" ? 1 : 0
   source = "./iam"
-
   bucket_name_full = local.bucket_name_full
   is_public_website = var.is_public_website
-}
-
-output "policy_document" {
-  value = module.iam.policy_document
 }
 
 module "resources" {
   count = local.workflow_step == "resources" ? 1 : 0
   source = "./resources"
-  depends_on = [time_sleep.wait_for_iam]
-
   bucket_name_full = local.bucket_name_full
   is_public_website = var.is_public_website
+}
+
+output "policy_document" {
+  value = module.generate_policy_document[0].policy_document
 }
