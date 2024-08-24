37499c09a2d1b77853011eee3f047dd5055725a1
carlssonk <o.carlssonk@gmail.com>
2024-08-24 08:46:57 +0200

    run

--- modules/s3/iam/main.tf
+++ modules/s3/iam/main.tf
@@ -27,9 +27,7 @@ data "aws_iam_policy_document" "this" {
     )
     resources = [
       "arn:aws:s3:::${var.bucket_name}",
-      "arn:aws:s3:::${var.bucket_name}/*",
-      "arn:aws:s3:::carlssonk-portfolio-prod/*",
-      "arn:aws:s3:::carlssonk-portfolio-prod",
+      "arn:aws:s3:::${var.bucket_name}/*"
     ]
     effect = "Allow"
   }
