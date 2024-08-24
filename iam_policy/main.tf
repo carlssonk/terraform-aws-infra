c063f559cf1eb69cdbb5392b55b579735f06766e
carlssonk <o.carlssonk@gmail.com>
2024-08-24 10:03:04 +0200

    Formatted logs

--- iam_policy/main.tf
+++ iam_policy/main.tf
@@ -65,10 +65,10 @@ resource "aws_iam_role_policy_attachment" "attachment" {
 output "policy_document" {
   value = {
 
-    one : tobool(module.globals.var.cleanup_policies),
-    two : try([for item in jsondecode(data.aws_iam_policy.previous_policy.policy) : jsonencode(item)], [])
-    three : try([for item in jsondecode(data.aws_iam_policy.previous_policy.policy) : item], [])
-    four : policies
+    one   = tobool(module.globals.var.cleanup_policies),
+    two   = try([for item in jsondecode(data.aws_iam_policy.previous_policy.policy) : jsonencode(item)], []),
+    three = try([for item in jsondecode(data.aws_iam_policy.previous_policy.policy) : item], []),
+    four  = policies
 
   }
   description = "The current set of policies, including both old and new"
