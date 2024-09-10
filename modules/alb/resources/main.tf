c484cbb671c01f8b5a1c63d0ff767930e5217661
carlssonk <o.carlssonk@gmail.com>
2024-09-10 03:46:33 +0200

    Create new target group

--- modules/alb/resources/main.tf
+++ modules/alb/resources/main.tf
@@ -51,6 +51,24 @@ resource "aws_lb_listener" "front_end" {
 
 resource "aws_lb_target_group" "ecs" {
   name        = "ecs-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "30"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/"
+    unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_lb_target_group" "ecs_new" {
+  name        = "ecs-target-group"
   port        = 3000
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
@@ -72,5 +90,5 @@ output "alb_dns_name" {
 }
 
 output "target_group_arn" {
-  value = aws_lb_target_group.ecs.arn
+  value = aws_lb_target_group.ecs_new.arn
 }
