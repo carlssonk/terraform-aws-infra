92b7c54899f2cc9f49ffd541556c9db0ac18cdb1
carlssonk <o.carlssonk@gmail.com>
2024-09-10 14:20:39 +0200

    Ref fix

--- modules/alb/resources/main.tf
+++ modules/alb/resources/main.tf
@@ -38,18 +38,7 @@ resource "aws_lb" "main" {
   enable_deletion_protection = false
 }
 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs.arn
-  }
-}
-
-resource "aws_lb_target_group" "ecs_new" {
+resource "aws_lb_target_group" "ecs" {
   name        = "ecs-target-group-new"
   port        = 3000 // TODO, set dynamicalle based on container port
   protocol    = "HTTP"
@@ -64,6 +53,17 @@ resource "aws_lb_target_group" "ecs_new" {
     timeout             = "3"
     path                = "/"
     unhealthy_threshold = "2"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs.arn
   }
 }
 
