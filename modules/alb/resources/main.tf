65bb1edfdb41ff5350fbbb61472ac4903f4f5e86
carlssonk <o.carlssonk@gmail.com>
2024-09-10 16:56:27 +0200

    Cleanup

--- modules/alb/resources/main.tf
+++ modules/alb/resources/main.tf
@@ -56,24 +56,6 @@ resource "aws_lb_target_group" "ecs" {
   }
 }
 
-resource "aws_lb_target_group" "ecs_new2" {
-  name        = "ecs-target-group-new2"
-  port        = 3000 // TODO, set dynamicalle based on container port
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "ip"
-
-  health_check {
-    healthy_threshold   = "3"
-    interval            = "30"
-    protocol            = "HTTP"
-    matcher             = "200"
-    timeout             = "3"
-    path                = "/"
-    unhealthy_threshold = "2"
-  }
-}
-
 resource "aws_lb_listener" "front_end" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
@@ -81,7 +63,7 @@ resource "aws_lb_listener" "front_end" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.ecs_new2.arn
+    target_group_arn = aws_lb_target_group.ecs.arn
   }
 }
 
@@ -90,5 +72,5 @@ output "alb_dns_name" {
 }
 
 output "target_group_arn" {
-  value = aws_lb_target_group.ecs_new2.arn
+  value = aws_lb_target_group.ecs.arn
 }
