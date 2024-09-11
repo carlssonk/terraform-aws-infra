2d048e899b58af2540f9aa74bb9147ddc32a6ae6
carlssonk <o.carlssonk@gmail.com>
2024-09-11 22:38:58 +0200

    Add repo

--- modules/ecs-service/resources/main.tf
+++ modules/ecs-service/resources/main.tf
@@ -57,17 +57,17 @@ resource "aws_ecs_service" "main" {
 }
 
 // Create one ECR repo per service
-# resource "aws_ecr_repository" "app_repo" {
-#   name                 = var.repo_name
-#   image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "app_repo" {
+  name                 = var.repo_name
+  image_tag_mutability = "MUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-#   encryption_configuration {
-#     encryption_type = "AES256"
-#   }
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
 
-#   force_delete = true
-# }
+  force_delete = true
+}
