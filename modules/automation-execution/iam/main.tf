0ce5e16ac659408cd4cc04c1b136e3178d6c9abb
carlssonk <o.carlssonk@gmail.com>
2024-09-11 15:31:54 +0200

    ssm:CreateAssociation

--- modules/automation-execution/iam/main.tf
+++ modules/automation-execution/iam/main.tf
@@ -13,6 +13,7 @@ data "aws_iam_policy_document" "this" {
         "ssm:CreateDocument",
         "ssm:DeleteDocument",
         "ssm:UpdateDocument",
+        "ssm:CreateAssociation",
         "ssm:Describe*",
         "ssm:Get*",
         "ssm:List*",
