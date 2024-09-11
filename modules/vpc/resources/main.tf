9b0a86782a93b21a538259cd80fc7a91fa182a03
carlssonk <o.carlssonk@gmail.com>
2024-09-11 21:19:15 +0200

    Fix ports

--- modules/vpc/resources/main.tf
+++ modules/vpc/resources/main.tf
@@ -117,8 +117,6 @@ resource "aws_vpc_security_group_ingress_rule" "allow_https" {
 resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
   security_group_id = aws_security_group.vpc_endpoints.id
 
-  from_port   = 0
-  to_port     = 0
   ip_protocol = "-1"
   cidr_ipv4   = "0.0.0.0/0"
 
