e284c0b8e4782f9208a4bd359edfc235fd1c85a8
carlssonk <o.carlssonk@gmail.com>
2024-09-11 14:48:45 +0200

    ssm:UpdateDocument

--- modules/automation-execution/iam/main.tf
+++ modules/automation-execution/iam/main.tf
@@ -8,6 +8,7 @@ data "aws_iam_policy_document" "this" {
       [
         "ssm:CreateDocument",
         "ssm:DeleteDocument",
+        "ssm:UpdateDocument",
         "ssm:Describe*",
         "ssm:Get*",
         "ssm:List*",
