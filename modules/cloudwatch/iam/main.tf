d8b28b3fc3cd7ff5a0a6f3fbd8d595e353e36ed3
carlssonk <o.carlssonk@gmail.com>
2024-09-11 02:45:56 +0200

    More permissions

--- modules/cloudwatch/iam/main.tf
+++ modules/cloudwatch/iam/main.tf
@@ -6,7 +6,12 @@ data "aws_iam_policy_document" "this" {
       [
         "logs:CreateLogGroup",
         "logs:CreateLogStream",
-        "logs:PutLogEvents"
+        "logs:DeleteLogGroup",
+        "logs:DeleteLogStream",
+        "logs:Put*",
+        "logs:List*",
+        "logs:Get*",
+        "logs:Describe*",
       ],
     )
     resources = ["*"]
