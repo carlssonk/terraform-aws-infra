9d3f287adb9b21d7edbbc461b3336785ac23e51d
carlssonk <o.carlssonk@gmail.com>
2024-09-11 14:57:37 +0200

    PassRole

--- modules/automation-execution/iam/main.tf
+++ modules/automation-execution/iam/main.tf
@@ -17,6 +17,16 @@ data "aws_iam_policy_document" "this" {
     resources = ["arn:aws:ssm:eu-north-1:${var.aws_account_id}:document/TroubleshootECSTaskFailedToStart"]
     effect    = "Allow"
   }
+
+  statement {
+    actions = concat(
+      [
+        "iam:PassRole"
+      ]
+    )
+    resources = ["arn:aws:iam::${var.aws_account_id}:role/terraform-execution-role"]
+    effect    = "Allow"
+  }
 }
 
 output "policy_document" {
