9d82021c3ee47d0711e225390fce2b2630a748d8
carlssonk <o.carlssonk@gmail.com>
2024-09-12 03:11:09 +0200

    443

--- modules/vpc/resources/main.tf
+++ modules/vpc/resources/main.tf
@@ -108,8 +108,8 @@ resource "aws_security_group" "vpc_endpoints" {
 resource "aws_vpc_security_group_ingress_rule" "allow_https" {
   security_group_id = aws_security_group.vpc_endpoints.id
 
-  to_port     = 0
-  from_port   = 0
+  to_port     = 443
+  from_port   = 443
   ip_protocol = "tcp"
   cidr_ipv4   = var.vpc_cidr
 
