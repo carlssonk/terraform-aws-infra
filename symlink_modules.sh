e01602547228d8dcb22c97fd428423671a3c0d73
carlssonk <o.carlssonk@gmail.com>
2024-09-16 20:48:38 +0200

    Fix symlink for outputs

--- symlink_modules.sh
+++ symlink_modules.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 
 STEP_NAME=${1:-resources}
+COMBINE_OUTPUTS=${2:-false}
 MODULES_DIR="./modules"
-
-# Function to remove existing default symlink or directory
-remove_existing_default() {
-    if [ -L "$module_dir/default" ] || [ -d "$module_dir/default" ]; then
-        rm -rf "$module_dir/default"
-    fi
-}
-
-# Function to create variables.tf symlink
-create_variables_symlink() {
-    if [ -f "$module_dir/variables.tf" ]; then
-        echo "    Creating variables.tf symlink in default"
-        ln -sf "../variables.tf" "$module_dir/default/variables.tf"
-    fi
-}
 
 # Check if the modules directory exists
 if [ ! -d "$MODULES_DIR" ]; then
@@ -46,26 +32,38 @@ for module_dir in "$MODULES_DIR"/*; do
         fi
         
         # Check if the specified step exists in this module
+        if [ -L "$module_dir/default" ] || [ -d "$module_dir/default" ]; then
+            rm -rf "$module_dir/default"
+        fi
+
+        combined_outputs=""
+        for subdir in "$module_dir"/*; do
+            if [ -d "$subdir" ]; then
+                output_file="$subdir/outputs.tf"
+                if [ -f "$output_file" ]; then
+                    combined_outputs+="# Outputs from $(basename "$subdir")\n"
+                    combined_outputs+="$(cat "$output_file")\n\n"
+                fi
+            fi
+        done
+
         if [ -d "$module_dir/$STEP_NAME" ]; then
             echo "  Creating symlink: default -> $STEP_NAME"
-            
-            remove_existing_default
-            
-            # Create new symlink
             ln -s "$STEP_NAME" "$module_dir/default"
-            
-            create_variables_symlink
         else
             echo "  Step '$STEP_NAME' not found in $module_name. Creating default directory."
-
-            remove_existing_default
-
-            # Create default directory and empty main.tf
-            mkdir -p "$module_dir/default"
+            mkdir "$module_dir/default"
             touch "$module_dir/default/main.tf"
             echo "    Created empty main.tf in default directory"
-            
-            create_variables_symlink
+            if [ -f "$module_dir/variables.tf" ]; then
+                echo "    Creating variables.tf symlink in default"
+                ln -sf "../variables.tf" "$module_dir/default/variables.tf"
+            fi
+        fi
+        
+        # Write combined_outputs default/outputs.tf (Only do this in CI pipeline since it will also create a outputs.tf in the corresponding symlinked folder as well which we dont need)
+        if [ "$COMBINE_OUTPUTS" = true ]; then
+            echo -e "$combined_outputs" > "$module_dir/default/outputs.tf"
         fi
     fi
 done
