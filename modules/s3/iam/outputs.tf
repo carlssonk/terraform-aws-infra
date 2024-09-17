90453598af0ddc5f1029a823e8d3817b3e1b5599
carlssonk <o.carlssonk@gmail.com>
2024-09-17 06:35:29 +0200

    Fix

--- modules/s3/iam/outputs.tf
+++ modules/s3/iam/outputs.tf
@@ -1,11 +1,3 @@
-# Outputs from iam
 output "policy_document" {
   value = data.aws_iam_policy_document.this.json
 }
-
-# Outputs from resources
-output "website_endpoint" {
-  value = null
-}
-
-
--- modules/vpc-endpoint/iam/outputs.tf
+++ modules/vpc-endpoint/iam/outputs.tf
@@ -1,6 +1,3 @@
-# Outputs from iam
 output "policy_document" {
   value = data.aws_iam_policy_document.this.json
 }
-
-
