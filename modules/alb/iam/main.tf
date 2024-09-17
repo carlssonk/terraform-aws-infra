17126ec21210dd87f142ca237cd00c58556cfd41
carlssonk <o.carlssonk@gmail.com>
2024-09-17 03:54:58 +0200

    Permissions

--- modules/alb/iam/main.tf
+++ modules/alb/iam/main.tf
@@ -10,6 +10,7 @@ data "aws_iam_policy_document" "this" {
         "elasticloadbalancing:*Listener*",
         "elasticloadbalancing:*Certificates*",
         "elasticloadbalancing:Describe*",
+        "elasticloadbalancing:CreateRule"
       ],
     )
     resources = [
