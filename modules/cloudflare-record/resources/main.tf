81942cdb05c6a6277900a8c08a6c4e264b2a5693
carlssonk <o.carlssonk@gmail.com>
2024-09-17 04:12:40 +0200

    Dont allow overwrite

--- modules/cloudflare-record/resources/main.tf
+++ modules/cloudflare-record/resources/main.tf
@@ -11,12 +11,11 @@ data "cloudflare_zone" "domain" {
 }
 
 resource "cloudflare_record" "dns_records" {
-  count           = length(var.dns_records)
-  zone_id         = data.cloudflare_zone.domain.id
-  name            = var.dns_records[count.index].name
-  content         = var.dns_records[count.index].value
-  type            = var.dns_records[count.index].type
-  ttl             = var.dns_records[count.index].ttl
-  proxied         = var.dns_records[count.index].proxied
-  allow_overwrite = true
+  count   = length(var.dns_records)
+  zone_id = data.cloudflare_zone.domain.id
+  name    = var.dns_records[count.index].name
+  content = var.dns_records[count.index].value
+  type    = var.dns_records[count.index].type
+  ttl     = var.dns_records[count.index].ttl
+  proxied = var.dns_records[count.index].proxied
 }
