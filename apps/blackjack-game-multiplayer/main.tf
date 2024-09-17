f3afc9aa2beb8f1360bd790e060d256626bffe5c
carlssonk <o.carlssonk@gmail.com>
2024-09-17 13:19:00 +0200

    module.cloudwatch.policy_document

--- apps/blackjack-game-multiplayer/main.tf
+++ apps/blackjack-game-multiplayer/main.tf
@@ -83,6 +83,7 @@ module "iam_policy" {
   policy_documents = [
     module.ecs_task_definition.policy_document,
     module.alb_target_group.policy_document,
-    module.ecs_service.policy_document
+    module.ecs_service.policy_document,
+    module.cloudwatch.policy_document
   ]
 }
