1be847c1d86d9d6b08f04be80208ce279e48cb78
carlssonk <o.carlssonk@gmail.com>
2024-09-24 01:03:54 +0200

    Vars

--- environments/staging-and-prod/variables.tf
+++ environments/staging-and-prod/variables.tf
@@ -3,12 +3,22 @@ variable "workflow_step" {
   type        = string
 }
 
+variable "cloudflare_api_token" {
+  description = "Cloudflare API token"
+  type        = string
+}
+
 variable "aws_region" {
   description = "AWS Region"
   type        = string
 }
 
-variable "cloudflare_api_token" {
-  description = "Cloudflare API token"
+variable "aws_account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+variable "organization" {
+  description = "Github account or organization name"
   type        = string
 }
