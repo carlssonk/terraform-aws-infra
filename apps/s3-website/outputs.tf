e440de5381d7e2658a00c391807d94e9be9d46cf
carlssonk <o.carlssonk@gmail.com>
2024-09-25 01:21:20 +0200

    Ref

--- apps/s3-website/outputs.tf
+++ apps/s3-website/outputs.tf
@@ -1,6 +1,6 @@
 output "policy_documents" {
   value = [
     module.subdomain_bucket.policy_document,
-    try(module.root_bucket.policy_document, null)
+    try(module.root_bucket[0].policy_document, null)
   ]
 }
