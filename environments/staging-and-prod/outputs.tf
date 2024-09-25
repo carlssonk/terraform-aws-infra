422d27dca51a1a1f3d2968203c21485f938f6ff8
carlssonk <o.carlssonk@gmail.com>
2024-09-25 18:33:44 +0200

    Replace with fargate_services_alb_policy

--- environments/staging-and-prod/outputs.tf
+++ environments/staging-and-prod/outputs.tf
@@ -1,8 +1,7 @@
 output "policy_documents" {
   value = {
-    (module.common_policy.name)      = module.common_policy.policy_document
-    (module.s3_websites_policy.name) = module.s3_websites_policy.policy_document
-    (module.blackjack_policy.name)   = module.blackjack_policy.policy_document
-    (module.flagracer_policy.name)   = module.flagracer_policy.policy_document
+    (module.common_policy.name)               = module.common_policy.policy_document
+    (module.s3_websites_policy.name)          = module.s3_websites_policy.policy_document
+    (module.fargate_services_alb_policy.name) = module.fargate_services_alb_policy.policy_document
   }
 }
