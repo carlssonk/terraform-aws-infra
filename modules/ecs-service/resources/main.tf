b8a545c61ff55e70d47edc7cbc1525156100aba8
carlssonk <o.carlssonk@gmail.com>
2024-09-25 22:39:34 +0200

    Removed launch_type

--- modules/ecs-service/resources/main.tf
+++ modules/ecs-service/resources/main.tf
@@ -10,7 +10,6 @@ resource "aws_ecs_service" "this" {
   cluster         = var.cluster_id
   task_definition = var.task_definition_arn
   desired_count   = 1
-  launch_type     = "FARGATE"
 
   network_configuration {
     subnets          = var.subnet_ids
