c60eb0cc65a086ee0012107172a249f4e25cca29
carlssonk <o.carlssonk@gmail.com>
2024-09-26 21:03:19 +0200

    Update

--- modules/service-discovery/iam/main.tf
+++ modules/service-discovery/iam/main.tf
@@ -6,8 +6,8 @@ data "aws_iam_policy_document" "this" {
         "servicediscovery:DeleteNamespace",
         "servicediscovery:GetNamespace",
         "servicediscovery:UpdateHttpNamespace",
-        "servicediscovery:CreatePrivateDnsNamespace",
-        "servicediscovery:UpdatePrivateDnsNamespace"
+        "servicediscovery:UpdatePrivateDnsNamespace",
+        "servicediscovery:CreatePrivateDnsNamespace"
       ]
     )
     resources = [
