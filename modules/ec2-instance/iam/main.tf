2b9a3c3d660bf9033d33f43de7d182a873ef3adc
carlssonk <o.carlssonk@gmail.com>
2024-09-26 02:02:26 +0200

    ec2:RunInstances -> *

--- modules/ec2-instance/iam/main.tf
+++ modules/ec2-instance/iam/main.tf
@@ -10,23 +10,11 @@ data "aws_iam_policy_document" "this" {
         "ec2:DescribeTags",
         "ec2:DescribeVolumes",
         "ec2:AttachVolume",
-        "ec2:DetachVolume"
+        "ec2:DetachVolume",
+        "ec2:RunInstances"
       ]
     )
     resources = ["*"]
     effect    = "Allow"
-  }
-
-  statement {
-    actions = concat(
-      [
-        "ec2:RunInstances"
-      ]
-    )
-    resources = [
-      "arn:aws:ec2:${module.globals.var.aws_region}:${module.globals.var.aws_account_id}:instance/*",
-      "arn:aws:ec2:${module.globals.var.aws_region}:${module.globals.var.aws_account_id}:network-interface/*"
-    ]
-    effect = "Allow"
   }
 }
