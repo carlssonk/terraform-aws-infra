b3ceaff7661676a7ee023cd1efdab7266caee876
carlssonk <o.carlssonk@gmail.com>
2024-09-26 03:26:52 +0200

    ec2:ModifyInstanceAttribute

--- modules/ec2-instance/iam/main.tf
+++ modules/ec2-instance/iam/main.tf
@@ -13,7 +13,8 @@ data "aws_iam_policy_document" "this" {
         "ec2:DetachVolume",
         "ec2:RunInstances",
         "ec2:ModifyNetworkInterfaceAttribute",
-        "ec2:StopInstances"
+        "ec2:StopInstances",
+        "ec2:ModifyInstanceAttribute"
       ]
     )
     resources = ["*"]
