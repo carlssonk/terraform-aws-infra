52e39a32c9feebfdbb1075b34c81eeddc93c2ef0
carlssonk <o.carlssonk@gmail.com>
2024-09-27 04:13:32 +0200

    ec2:StartInstances

--- modules/ec2-instance/iam/main.tf
+++ modules/ec2-instance/iam/main.tf
@@ -15,7 +15,8 @@ data "aws_iam_policy_document" "this" {
         "ec2:ModifyNetworkInterfaceAttribute",
         "ec2:StopInstances",
         "ec2:ModifyInstanceAttribute",
-        "ec2:TerminateInstances"
+        "ec2:TerminateInstances",
+        "ec2:StartInstances"
       ]
     )
     resources = ["*"]
