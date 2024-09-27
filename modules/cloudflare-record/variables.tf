b99b261a4f58f009ab6000ce1c72b3a4900937ee
carlssonk <o.carlssonk@gmail.com>
2024-09-27 05:10:08 +0200

    Fix var

--- modules/cloudflare-record/variables.tf
+++ modules/cloudflare-record/variables.tf
@@ -5,11 +5,11 @@ variable "root_domain" {
 
 variable "dns_records" {
   description = "List of DNS records to create"
-  type = map({
+  type = map(object({
     name    = string
     value   = string
     type    = optional(string, "CNAME")
     ttl     = optional(number, 1)
     proxied = optional(bool, true)
-  })
+  }))
 }
