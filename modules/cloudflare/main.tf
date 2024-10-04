5f8907c7de3b7ef3c6297005d65399409c71e8a7
carlssonk <o.carlssonk@gmail.com>
2024-10-04 18:23:53 +0200

    Use terraform

--- modules/cloudflare/main.tf
+++ modules/cloudflare/main.tf
@@ -36,85 +36,73 @@ data "cloudflare_zone" "domain" {
   name     = each.key
 }
 
-// Resource can only be created once globally across all environments
-# resource "cloudflare_zone_settings_override" "this" {
-#   for_each = local.apps_grouped_by_root_domain
-#   zone_id  = data.cloudflare_zone.domain[each.key].id
-
-#   settings {
-#     ssl              = "full"
-#     always_use_https = "on"
-#   }
-# }
-
-resource "null_resource" "cloudflare_zone_settings_override" {
+// Resource will affect all environments
+resource "cloudflare_zone_settings_override" "this" {
   for_each = local.apps_grouped_by_root_domain
+  zone_id  = data.cloudflare_zone.domain[each.key].id
 
-  triggers = {
-    cloudflare_api_token = var.cloudflare_api_token
-    zone_id              = data.cloudflare_zone.domain[each.key].id
-    ssl                  = "full"
-    always_use_https     = "on"
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/cloudflare_zone_settings_override.sh"
-    environment = {
-      CLOUDFLARE_API_TOKEN = self.triggers.cloudflare_api_token
-      ZONE_ID              = self.triggers.zone_id
-      SSL                  = self.triggers.ssl
-      ALWAYS_USE_HTTPS     = self.triggers.always_use_https
-    }
+  settings {
+    ssl              = "full"
+    always_use_https = "on"
   }
 }
 
-// Resource can only be created once globally across all environments
-# resource "cloudflare_ruleset" "this" {
-#   for_each    = local.apps_grouped_by_root_domain
-#   zone_id     = data.cloudflare_zone.domain[each.key].id
-#   name        = "Dynamic Main Ruleset"
-#   description = "Dynamic ruleset for managing app settings"
-#   kind        = "zone"
-#   phase       = "http_config_settings"
+resource "cloudflare_ruleset" "prod" {
+  for_each    = terraform.workspace == "prod" ? local.apps_grouped_by_root_domain : []
+  zone_id     = data.cloudflare_zone.domain[each.key].id
+  name        = "Dynamic Main Ruleset PROD"
+  description = "Dynamic ruleset for managing app settings"
+  kind        = "zone"
+  phase       = "http_config_settings"
 
-#   dynamic "rules" {
-#     for_each = local.ruleset_rules[each.key]
-#     content {
-#       action = rules.value.action
+  dynamic "rules" {
+    for_each = local.ruleset_rules[each.key]
+    content {
+      action = rules.value.action
 
-#       dynamic "action_parameters" {
-#         for_each = rules.value.action_parameters.ssl != null ? [rules.value.action_parameters.ssl] : []
-#         content {
-#           ssl = action_parameters.value
-#         }
-#       }
+      dynamic "action_parameters" {
+        for_each = rules.value.action_parameters.ssl != null ? [rules.value.action_parameters.ssl] : []
+        content {
+          ssl = action_parameters.value
+        }
+      }
 
-#       expression  = rules.value.expression
-#       description = rules.value.description
-#     }
-#   }
-# }
-
-resource "null_resource" "cloudflare_ruleset" {
-  for_each = local.apps_grouped_by_root_domain
-
-  triggers = {
-    cloudflare_api_token = var.cloudflare_api_token
-    zone_id              = data.cloudflare_zone.domain[each.key].id
-    kind                 = "zone"
-    phase                = "http_config_settings"
-    ruleset_rules        = jsonencode(local.ruleset_rules[each.key])
-    temp                 = false
-  }
-
-  provisioner "local-exec" {
-    command = "${path.module}/cloudflare_ruleset.sh"
-    environment = {
-      CLOUDFLARE_API_TOKEN = self.triggers.cloudflare_api_token
-      ZONE_ID              = self.triggers.zone_id
-      KIND                 = self.triggers.kind
-      PHASE                = self.triggers.phase
-      RULESET_RULES        = self.triggers.ruleset_rules
+      expression  = rules.value.expression
+      description = rules.value.description
     }
   }
+
+  # lifecycle {
+  #   ignore_changes = terraform.workspace == "prod" ? all : []
+  # }
+}
+
+resource "cloudflare_ruleset" "staging" {
+  for_each    = terraform.workspace == "staging" ? local.apps_grouped_by_root_domain : []
+  zone_id     = data.cloudflare_zone.domain[each.key].id
+  name        = "Dynamic Main Ruleset STAGING"
+  description = "Dynamic ruleset for managing app settings"
+  kind        = "zone"
+  phase       = "http_config_settings"
+
+  dynamic "rules" {
+    for_each = local.ruleset_rules[each.key]
+    content {
+      action = rules.value.action
+
+      dynamic "action_parameters" {
+        for_each = rules.value.action_parameters.ssl != null ? [rules.value.action_parameters.ssl] : []
+        content {
+          ssl = action_parameters.value
+        }
+      }
+
+      expression  = rules.value.expression
+      description = rules.value.description
+    }
+  }
+
+  # lifecycle {
+  #   ignore_changes = terraform.workspace == "staging" ? all : []
+  # }
 }
