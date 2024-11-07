b741f88789116aa76796f86c054cb8a4a397238a
carlssonk <o.carlssonk@gmail.com>
2024-11-07 22:09:13 +0100

    Index

--- environments/staging-and-prod/outputs.tf
+++ environments/staging-and-prod/outputs.tf
@@ -1,10 +1,10 @@
 output "policy_documents" {
   value = {
-    (module.networking_policy.name)       = module.networking_policy.policy_document
-    (module.security_policy.name)         = module.security_policy.policy_document
-    (module.services_policy.name)         = module.services_policy.policy_document
-    (module.s3_websites_policy.name)      = module.s3_websites_policy.policy_document
-    (module.s3_media_policy.name)         = module.s3_media_policy.policy_document
-    (module.fargate_services_policy.name) = module.fargate_services_policy.policy_document
+    (module.networking_policy.name)          = module.networking_policy.policy_document
+    (module.security_policy.name)            = module.security_policy.policy_document
+    (module.services_policy.name)            = module.services_policy.policy_document
+    (module.s3_websites_policy[0].name)      = module.s3_websites_policy[0].policy_document
+    (module.s3_media_policy[0].name)         = module.s3_media_policy[0].policy_document
+    (module.fargate_services_policy[0].name) = module.fargate_services_policy[0].policy_document
   }
 }
