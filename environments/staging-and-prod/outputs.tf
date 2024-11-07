150a67c0f052c9949acee57f253549ad8d3b5558
carlssonk <o.carlssonk@gmail.com>
2024-11-07 22:12:03 +0100

    Conditionally output

--- environments/staging-and-prod/outputs.tf
+++ environments/staging-and-prod/outputs.tf
@@ -1,10 +1,18 @@
 output "policy_documents" {
-  value = {
-    (module.networking_policy.name)          = module.networking_policy.policy_document
-    (module.security_policy.name)            = module.security_policy.policy_document
-    (module.services_policy.name)            = module.services_policy.policy_document
-    (module.s3_websites_policy[0].name)      = module.s3_websites_policy[0].policy_document
-    (module.s3_media_policy[0].name)         = module.s3_media_policy[0].policy_document
-    (module.fargate_services_policy[0].name) = module.fargate_services_policy[0].policy_document
-  }
+  value = merge(
+    {
+      (module.networking_policy.name) = module.networking_policy.policy_document
+      (module.security_policy.name)   = module.security_policy.policy_document
+      (module.services_policy.name)   = module.services_policy.policy_document
+    },
+    length(module.s3_websites_policy) > 0 ? {
+      (module.s3_websites_policy[0].name) = module.s3_websites_policy[0].policy_document
+    } : {},
+    length(module.s3_media_policy) > 0 ? {
+      (module.s3_media_policy[0].name) = module.s3_media_policy[0].policy_document
+    } : {},
+    length(module.fargate_services_policy) > 0 ? {
+      (module.fargate_services_policy[0].name) = module.fargate_services_policy[0].policy_document
+    } : {}
+  )
 }
